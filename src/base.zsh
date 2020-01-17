2c38589e1a6a29122053b5d20bd62243e40ff578
Luis Mayta <luis@hadenlabs.com>
2020-01-17 15:32:35 -0500

    chore: Implement dependence hub (#2)

--- src/base.zsh
+++ src/base.zsh
@@ -1,2 +1,13 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
+
+function hub::install {
+    if ! type -p brew > /dev/null; then
+        message_warning "${ISSUES_MESSAGE_REQUIRED_BREW}"
+        return
+    fi
+
+    brew install hub
+}
+
+if ! type -p hub > /dev/null; then hub::install; fi
