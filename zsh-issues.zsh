405d54607fd23d83839812a12ce42f407b1c0331
@slovacus <slovacus@gmail.com>
2020-01-17 15:35:10 -0500

    (#2) docs: Implement info for brew required

--- zsh-issues.zsh
+++ zsh-issues.zsh
@@ -12,6 +12,9 @@ issues_package_name="issues"
 ISSUES_PLUGIN_DIR=$(dirname "${0}":A)
 ISSUES_SOURCE_PATH="${ISSUES_PLUGIN_DIR}"/src
 
+# messages
+ISSUES_MESSAGE_REQUIRED_BREW="it's neccesary have brew for install or add luismayta/zsh-brew"
+
 # shellcheck source=/dev/null
 source "${ISSUES_SOURCE_PATH}"/base.zsh
 
