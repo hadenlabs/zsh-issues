5eb74158651bf1eace5a7b015dcbbb1077db349c
@slovacus <slovacus@gmail.com>
2020-03-07 21:26:34 -0500

    style: fix format eslintrc and lintstagedrc (#7)

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,11 +2,8 @@ const escape = require('shell-quote').quote;
 
 module.exports = {
   '*.{ts,tsx,js,json,css}': (filenames) => [
-    ...filenames.map(
-      (filename) =>
-        `prettier --check "${escape([filename])}"`
-    ),
-    ...filenames.map((filename) => `git add "${filename}"`)
+    ...filenames.map((filename) => `prettier --check "${escape([filename])}"`),
+    ...filenames.map((filename) => `git add "${filename}"`),
   ],
-  '*.{ts,tsx,js,jsx}': ['eslint']
+  '*.{ts,tsx,js,jsx}': ['eslint'],
 };
