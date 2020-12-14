babba434b9d8b2d867ac44b31464e16744e00ebe
@slovacus <slovacus@gmail.com>
2020-12-14 15:43:53 -0500

    refactor: load file for issues create (#15)

---
+++ internal/main.zsh
@@ -0,0 +1,24 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+function issues::internal::main::factory {
+    # shellcheck source=/dev/null
+    source "${ZSH_ISSUES_PATH}"/internal/base.zsh
+    case "${OSTYPE}" in
+    darwin*)
+        # shellcheck source=/dev/null
+        source "${ZSH_ISSUES_PATH}"/internal/osx.zsh
+        ;;
+    linux*)
+        # shellcheck source=/dev/null
+        source "${ZSH_ISSUES_PATH}"/internal/linux.zsh
+      ;;
+    esac
+    # shellcheck source=/dev/null
+    source "${ZSH_ISSUES_PATH}"/internal/helper.zsh
+}
+
+issues::internal::main::factory
+
+if ! type -p rsync > /dev/null; then issues::internal::rync::install; fi
+if ! type -p hub > /dev/null; then issues::internal::hub::install; fi
---
+++ provider/main.zsh
@@ -0,0 +1,25 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+function issues::provider::main::factory {
+    # shellcheck source=/dev/null
+    source "${ZSH_ISSUES_PATH}"/provider/base.zsh
+    case "${OSTYPE}" in
+    darwin*)
+        # shellcheck source=/dev/null
+        source "${ZSH_ISSUES_PATH}"/provider/osx.zsh
+        ;;
+    linux*)
+        # shellcheck source=/dev/null
+        source "${ZSH_ISSUES_PATH}"/provider/linux.zsh
+      ;;
+    esac
+
+    # shellcheck source=/dev/null
+    source "${ZSH_ISSUES_PATH}"/provider/github.zsh
+
+    # shellcheck source=/dev/null
+    source "${ZSH_ISSUES_PATH}"/provider/alias.zsh
+}
+
+issues::provider::main::factory
