14423f3e4ad4b12c9d47327b07752ecaca1869d5
Luis Mayta <luis@hadenlabs.com>
2020-12-14 15:58:21 -0500

    refactor: change hub to gh install (#21)

--- internal/base.zsh
+++ internal/base.zsh
@@ -1,14 +1,14 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-function issues::internal::hub::install {
-    message_info "Installing ${ISSUES_PACKAGE_NAME}"
+function issues::internal::gh::install {
+    message_info "Installing gh ${ISSUES_PACKAGE_NAME}"
     if ! type -p brew > /dev/null; then
         message_warning "${ISSUES_MESSAGE_BREW}"
         return
     fi
-    brew install hub
-    message_success "Installed ${ISSUES_PACKAGE_NAME}"
+    brew install gh
+    message_success "Installed gh ${ISSUES_PACKAGE_NAME}"
 }
 
 function issues::internal::rsync::install {
