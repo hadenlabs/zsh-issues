5e9e740ade9cd6a7dd894d8dfcf44a17fe69443c
@slovacus <slovacus@gmail.com>
2020-12-14 21:02:08 -0500

    refactor: settings for config (#9)

--- config/base.zsh
+++ config/base.zsh
@@ -1,10 +1,7 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-export ISSUES_FILE_SETTINGS="${HOME}"/.tmux.conf
 export ISSUES_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
 export ISSUES_MESSAGE_RVM="Please install rvm or use antibody bundle luismayta/zsh-rvm branch:develop"
-export ISSUES_PACKAGE_NAME=tmux
-export ISSUES_TPM_PATH="${HOME}"/.tmux/plugins/tpm
-
-[ -z "${EDITOR}" ] && export EDITOR="vim"
+export ISSUES_TEMPLATES_PATH="${ZSH_ISSUES_PATH}/resources"
+export ISSUES_PACKAGE_NAME=issues
