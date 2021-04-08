82cc12f3ef36044de7163d05f5547ac48a9c31f0
@slovacus <slovacus@gmail.com>
2021-04-08 10:02:19 -0500

    chore: implement var issues search args (#33)

--- config/base.zsh
+++ config/base.zsh
@@ -4,4 +4,5 @@
 export ISSUES_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
 export ISSUES_MESSAGE_RVM="Please install rvm or use antibody bundle luismayta/zsh-rvm branch:develop"
 export ISSUES_TEMPLATES_PATH="${ZSH_ISSUES_PATH}/resources"
+export ISSUES_SEARCH_ARGS=""
 export ISSUES_PACKAGE_NAME=issues
