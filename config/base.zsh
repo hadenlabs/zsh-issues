6cb46795094129092b82459e07eeee96877efbf3
luismayta <luis@hadenlabs.com>
2021-09-20 10:46:44 -0500

    chore: implement git remote url (#62)

--- config/base.zsh
+++ config/base.zsh
@@ -6,3 +6,6 @@ export ISSUES_MESSAGE_RVM="Please install rvm or use antibody bundle luismayta/z
 export ISSUES_TEMPLATES_PATH="${ZSH_ISSUES_PATH}/resources"
 export ISSUES_SEARCH_ARGS=""
 export ISSUES_PACKAGE_NAME=issues
+
+GIT_REMOTE=$(command git ls-remote --get-url 2> /dev/null)
+export GIT_REMOTE
