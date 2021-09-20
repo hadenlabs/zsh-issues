42afe29e850230356efe439ba69dbc584cadbee2
luismayta <luis@hadenlabs.com>
2021-09-20 10:50:20 -0500

    chore: implement get provider function (#62)

--- internal/base.zsh
+++ internal/base.zsh
@@ -13,6 +13,20 @@ function issues::internal::git::branch::name {
     git symbolic-ref --short HEAD
 }
 
+function issues::internal::get::provider {
+    local provider_name
+    provider_name="github"
+    if [[ "$GIT_REMOTE" =~ "github" ]]; then
+        provider_name="github"
+    elif [[ "$GIT_REMOTE" =~ "gitlab" ]]; then
+        provider_name="gitlab"
+    elif [[ "$GIT_REMOTE" =~ "bitbucket" ]]; then
+        provider_name="bitbucket"
+    fi
+    echo "${provider_name}"
+}
+
+
 function issues::internal::git::branch::kind {
     issues::internal::git::branch::name | cut -d "/" -f 1
 }
