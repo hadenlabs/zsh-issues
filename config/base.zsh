023797af8c8b303fc4baa5b51af1a46a33a72c37
Luis Mayta <luis@hadenlabs.com>
2023-09-23 16:09:43 -0300

    fix: package dependencies for zsh (#94)

--- config/base.zsh
+++ config/base.zsh
@@ -1,8 +1,8 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-export ISSUES_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
-export ISSUES_MESSAGE_RVM="Please install rvm or use antibody bundle luismayta/zsh-rvm branch:develop"
+export ISSUES_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew"
+export ISSUES_MESSAGE_RVM="Please install rvm or use antibody bundle luismayta/zsh-rvm"
 export ISSUES_TEMPLATES_PATH="${ZSH_ISSUES_PATH}/resources"
 export ISSUES_SEARCH_ARGS=""
 export ISSUES_PACKAGE_NAME=issues
